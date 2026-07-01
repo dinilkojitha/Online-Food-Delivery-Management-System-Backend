c2380111587257b16393959dcd403339f0e119c4
IT25103205 <dinilkojitha@gmail.com>
2026-07-01 21:38:24 +0530

    Initial commit

---
+++ src/main/java/com/example/online_food_delivery_management_system_backend/OnlineFoodDeliveryManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.online_food_delivery_management_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFoodDeliveryManagementSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineFoodDeliveryManagementSystemBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/online_food_delivery_management_system_backend/OnlineFoodDeliveryManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_food_delivery_management_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodDeliveryManagementSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
